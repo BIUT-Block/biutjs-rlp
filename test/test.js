650b50b0db46bd7795cb93aad97ee3c382dc713e
Zu <zuweihan1992@gmail.com>
2018-05-21 15:11:22 +0200

    change to ES6 standard format

--- test/test.js
+++ test/test.js
@@ -7,37 +7,39 @@ const fs = require("fs")
 
 
 test('SEC RLP test', t => {
+	let rlpEncode = new rlp()
+	
 	t.plan(4)
 	var contents = fs.readFileSync("test_json.json")
 	var ebook = fs.readFileSync("ebook_genesisBlock.json")
 	var sec_block = fs.readFileSync("sec_blockchain-transaction-model.json")
 	var sec_shop = fs.readFileSync("sec_shop-transaction-model.json")
 	
-	contents_rlp_encode = rlp.jsonToRlp(contents)
-	contents_json_format = rlp.jsonKeyArray(contents)
+	contents_rlp_encode = rlpEncode.jsonToRlp(contents)
+	contents_json_format = rlpEncode.jsonKeyArray(contents)
 	
-	ebook_rlp_encode = rlp.jsonToRlp(ebook)
-	ebook_json_format = rlp.jsonKeyArray(ebook)
+	ebook_rlp_encode = rlpEncode.jsonToRlp(ebook)
+	ebook_json_format = rlpEncode.jsonKeyArray(ebook)
 	
-	sec_block_rlp_encode = rlp.jsonToRlp(sec_block)
-	sec_block_json_format = rlp.jsonKeyArray(sec_block)
+	sec_block_rlp_encode = rlpEncode.jsonToRlp(sec_block)
+	sec_block_json_format = rlpEncode.jsonKeyArray(sec_block)
 	
-	sec_shop_rlp_encode = rlp.jsonToRlp(sec_shop)
-	sec_shop_json_format = rlp.jsonKeyArray(sec_shop)
+	sec_shop_rlp_encode = rlpEncode.jsonToRlp(sec_shop)
+	sec_shop_json_format = rlpEncode.jsonKeyArray(sec_shop)
 	
-	//js_sec_block_rlp_encode = rlp.jsonToRlp(sec_block_js)
-	//js_sec_block_json_format = rlp.jsonKeyArray(sec_block_js)
+	//js_sec_block_rlp_encode = rlpEncode.jsonToRlp(sec_block_js)
+	//js_sec_block_json_format = rlpEncode.jsonKeyArray(sec_block_js)
 	
-	//js_sec_shop_rlp_encode = rlp.jsonToRlp(sec_shop_js)
-	//js_sec_shop_json_format = rlp.jsonKeyArray(sec_shop_js)
+	//js_sec_shop_rlp_encode = rlpEncode.jsonToRlp(sec_shop_js)
+	//js_sec_shop_json_format = rlpEncode.jsonKeyArray(sec_shop_js)
 	
 	if(0){
 		console.log("--------------------------")
-		console.log(rlp.decode(ebook_rlp_encode))
+		console.log(rlpEncode.decode(ebook_rlp_encode))
 		console.log("--------------------------")
 		console.log(ebook_json_format)
 		console.log("--------------------------")
-		console.log(JSON.parse(rlp.rlpToJson(ebook_rlp_encode, ebook_json_format)))
+		console.log(JSON.parse(rlpEncode.rlpToJson(ebook_rlp_encode, ebook_json_format)))
 		console.log("--------------------------")
 		console.log(JSON.parse(ebook))
 		console.log("--------------------------")
@@ -47,20 +49,20 @@ test('SEC RLP test', t => {
 		console.log("--------------------------")
 		console.log(contents_rlp_encode)
 		console.log("--------------------------")
-		console.log(rlp.decode(contents_rlp_encode))
+		console.log(rlpEncode.decode(contents_rlp_encode))
 		console.log("--------------------------")
 		console.log(contents_json_format)
 		console.log("--------------------------")
 	}
 	
-	t.deepEqual(JSON.parse(rlp.rlpToJson(contents_rlp_encode, contents_json_format)), JSON.parse(contents))
-	t.deepEqual(JSON.parse(rlp.rlpToJson(ebook_rlp_encode, ebook_json_format)), JSON.parse(ebook))
+	t.deepEqual(JSON.parse(rlpEncode.rlpToJson(contents_rlp_encode, contents_json_format)), JSON.parse(contents))
+	t.deepEqual(JSON.parse(rlpEncode.rlpToJson(ebook_rlp_encode, ebook_json_format)), JSON.parse(ebook))
 	
-	t.deepEqual(JSON.parse(rlp.rlpToJson(sec_block_rlp_encode, sec_block_json_format)), JSON.parse(sec_block))
-	t.deepEqual(JSON.parse(rlp.rlpToJson(sec_shop_rlp_encode, sec_shop_json_format)), JSON.parse(sec_shop))
+	t.deepEqual(JSON.parse(rlpEncode.rlpToJson(sec_block_rlp_encode, sec_block_json_format)), JSON.parse(sec_block))
+	t.deepEqual(JSON.parse(rlpEncode.rlpToJson(sec_shop_rlp_encode, sec_shop_json_format)), JSON.parse(sec_shop))
 	
-	//t.deepEqual(JSON.parse(rlp.rlpToJson(js_sec_block_rlp_encode, js_sec_block_json_format)), JSON.parse(sec_block))
-	//t.deepEqual(JSON.parse(rlp.rlpToJson(js_sec_shop_rlp_encode, js_sec_shop_json_format)), JSON.parse(sec_shop))
+	//t.deepEqual(JSON.parse(rlpEncode.rlpToJson(js_sec_block_rlp_encode, js_sec_block_json_format)), JSON.parse(sec_block))
+	//t.deepEqual(JSON.parse(rlpEncode.rlpToJson(js_sec_shop_rlp_encode, js_sec_shop_json_format)), JSON.parse(sec_shop))
 })
 
 
